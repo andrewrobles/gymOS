742537247d1ed9154ebfe9c361e29297ad609794
Andrew Robles <andrewrobles@verizon.net>
2022-11-25 15:44:40 -0800

    Format indentation

--- ui/App.js
+++ ui/App.js
@@ -4,61 +4,80 @@ import { useState, useEffect } from 'react'
 import { TextInput } from 'react-native';
 
 const ExerciseList = () => {
-  const [exercises, setExercises] = useState([])
+	const [exercises, setExercises] = useState([])
 
-  useEffect(() => {
-    const requestOptions = {
-      method: 'GET',
-      headers: { 'Content-Type': 'application/json' },
-      };
-      fetch('http://localhost:5000/exercise', requestOptions)
-          .then(response => response.json())
-          .then((data) => setExercises(data))
-  })
+	useEffect(() => {
+		const requestOptions = {
+			method: 'GET',
+			headers: { 'Content-Type': 'application/json' },
+		};
+		fetch('http://localhost:5000/exercise', requestOptions)
+		.then(response => response.json())
+		.then((data) => setExercises(data))
+	})
 
-  const listItems = exercises.map((exercise) => <Text key={exercise.name}>{exercise.name}</Text> )
-  return <View>{listItems}</View>
+	const listItems = exercises.map((exercise) => <Text key={exercise.name}>{exercise.name}</Text>)
+	return <View>{listItems}</View>
 }
 
 const AddExerciseForm = () => {
-  const [textInputValue, setTextInputValue] = useState('');
+	const [textInputValue, setTextInputValue] = useState('');
 
-  const submitExerciseForm = (exerciseName) => {
-    const requestOptions = {
-      method: 'POST',
-      headers: { 'Content-Type': 'application/json' },
-      body: JSON.stringify({ "name": exerciseName})
-      };
-      fetch('http://localhost:5000/exercise/add', requestOptions)
-          .then(response => response.json())
-  }
+	const submitExerciseForm = (exerciseName) => {
+		const requestOptions = {
+			method: 'POST',
+			headers: { 'Content-Type': 'application/json' },
+			body: JSON.stringify({ "name": exerciseName})
+		};
+		fetch('http://localhost:5000/exercise/add', requestOptions)
+		.then(response => response.json())
+	}
 
-  return (
-    <View>
-    <TextInput
-    style={{ 
-    height: 40, 
-    borderColor: 'gray', 
-    borderWidth: 1,
-    placeholderTextColor: 'gray',
-  }}
-    onChangeText={text => setTextInputValue(text)}
-    value={textInputValue}
-  placeholder="Insert your text!"
-  />
-  <Button
-  title="Press me"
-  onPress={() => submitExerciseForm(textInputValue)}/>
-  </View>
+	return (
+		<View>
+			<TextInput
+			style={{
+				height: 40, 
+				borderColor: 'gray', 
+				borderWidth: 1,
+				placeholderTextColor: 'gray',
+			}}
+			onChangeText={text => setTextInputValue(text)}
+			value={textInputValue}
+			placeholder="Insert your text!"/>
+			<Button
+			title="Save"
+			onPress={() => submitExerciseForm(textInputValue)}/>
+		</View>
   );
 }
 
+const Navbar = () => {
+	return (
+		<View>
+		<Button
+		title="Workouts"/>
+		<Button
+		title="Exercises"/>
+		</View>
+	)
+}
+
+const ExerciseView = () => {
+	return <View>
+		<AddExerciseForm/>
+		<ExerciseList/>
+	</View>
+}
+
+
+
 export default function App() {
   return (
-    <View style={styles.container}>
-      <AddExerciseForm/>
-      <ExerciseList/>
-    </View>
+	<View style={styles.container}>
+		<ExerciseView/>
+		<Navbar/>	
+	</View>
   );
 }
 
@@ -66,7 +85,6 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    margin: 30,
   },
 });
