e21bdb2570ddadeb070c6d36fa053a67c8384ba6
Andrew Robles <andrewrobles@verizon.net>
2022-12-03 16:49:48 -0800

    Swagger autogen correct port

--- api/swagger.js
+++ api/swagger.js
@@ -1,6 +1,9 @@
 const swaggerAutogen = require('swagger-autogen')()
 
+doc = {host: 'localhost:5000'}
 const outputFile = './swagger_output.json'
 const endpointsFiles = ['./routes/record.js']
 
-swaggerAutogen(outputFile, endpointsFiles)
+swaggerAutogen(outputFile, endpointsFiles, doc).then(() => {
+    require('./server.js')
+})
