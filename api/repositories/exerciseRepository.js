6f47fa4c51ad4a1ce3f3a0e2cf7bac8b7f45776a
Andrew Robles <andrewrobles@verizon.net>
2022-12-08 14:50:54 -0800

    Remove exerciseRepository

--- api/repositories/exerciseRepository.js
+++
@@ -1,7 +0,0 @@
-const { collection } = require('__mocks__/db')
-const base = require('baseRepository.js')
-
-const exerciseCollection = collection('exercises')
-const exerciseRepository = base.repository(exerciseCollection)
-
-module.exports = exerciseRepository
