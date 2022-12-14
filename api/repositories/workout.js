00577672882cf145c593aca755c086e4789589c3
Andrew Robles <andrewrobles@verizon.net>
2022-12-14 04:33:21 -0800

    Implement workoutRepository.addExercise

--- api/repositories/workout.js
+++ api/repositories/workout.js
@@ -1,3 +1,18 @@
 const base = require('./base')
 
-module.exports = (db) => base.repository(db.collection('workouts'))
+module.exports = (db) => {
+	const workoutRepository = base.repository(db.collection('workouts'))
+	const exerciseRepository = base.repository(db.collection('exercises'))
+	const addExercise = async (workoutId, exerciseId) => {
+		const [,workout] = await workoutRepository.findOneById(workoutId)
+		const [,exercise] = await exerciseRepository.findOneById(exerciseId)
+		workout.exercises.push(exerciseId)
+		exercise.workouts.push(workoutId)
+		await workoutRepository.updateOneById(workoutId, workout)
+		await exerciseRepository.updateOneById(exerciseId, exercise)
+	}
+	return {
+		...workoutRepository,
+		addExercise
+	}
+}
