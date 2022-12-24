b1cde8c918fa467a7e4d34fa5d5c23d1000a71d6
Andrew Robles <andrewrobles@verizon.net>
2022-12-24 12:34:25 -0800

    Rename Exercise.js and Workout.js to ExerciseView.js and WorkoutView.js

--- ui/App.js
+++ ui/App.js
@@ -1,11 +1,11 @@
-import Exercises from './components/Exercises.js'
-import Workouts from './components/Workouts.js'
+import ExerciseView from './components/ExerciseView.js'
+import WorkoutView from './components/WorkoutView.js'
 import Navbar from './components/Navbar.js'
 
 export default function App() {
 	const views = {
-		Exercises: <Exercises/>,
-		Workouts: <Workouts/>,
+		Exercises: <ExerciseView/>,
+		Workouts: <WorkoutView/>,
 	}
 	return <Navbar views={views} defaultView={'Exercises'}/>
 }
