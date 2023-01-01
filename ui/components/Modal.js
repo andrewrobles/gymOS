a113986af9e35fbd7da42ee8fc5fb3a22f611929
Andrew Robles <andrewrobles@verizon.net>
2022-12-31 22:08:16 -0800

    Refactor modal

--- ui/components/Modal.js
+++ ui/components/Modal.js
@@ -2,15 +2,16 @@ import { useState, useEffect } from 'react'
 import { Button, View, Text } from 'react-native'
 
 export default function Modal(props) {
+const hideModal = () => props.showModal(false)
 return <View>
-{props.modalIsVisible ?
-<Button 
-	title="Go Back" 
-	onPress={() => props.showModal(false)}
-/>: null}
+{props.modalIsVisible ? 
+<Button title="Go Back" onPress={hideModal}/>: 
+null}
+
 {!props.modalIsVisible && props.buttonText !== undefined ?
-<Button title={props.buttonText} onPress={props.showModal}/>
-: null}
+<Button title={props.buttonText} onPress={props.showModal}/>:
+null}
+
 {props.modalIsVisible ? props.content: null}
 </View>
 }
